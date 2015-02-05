07565571997624a3c1d479438f7d2626738a2c5a
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2015-02-05 10:08:40 +0100

    params datei...

---
+++ params.pp
@@ -0,0 +1,16 @@
+class owncloud::params(
+	$owncloud_db_password=undef,
+	$owncloud_node_ips=undef,
+	$root_db_password=undef,
+	$passwordsalt=undef,
+	$secret=undef,
+	$trusted_domains=undef,
+	$datadirectory=undef,
+	$baseurl=undef,
+	$dbtype=undef,
+	$dbname=undef,
+	dbhost=undef,
+	$dbtableprefix='oc_',
+	$dbuser=undef
+) {
+}
