e8cadc7c37f6e0198cece67e8d763e523663ccfc
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2015-03-12 11:09:15 +0100

    removed depndency

--- manifests/appnode.pp
+++ manifests/appnode.pp
@@ -235,6 +235,6 @@ class owncloud::appnode(
     user => 'batman',
     type => 'ssh-rsa',
     key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQorL7vdCrom0bMA5marc4uAWMndhLKlzLTXYsHifiqJB6h1NLUesE5ovuY0iI9Zs4evD58dcQC5KRwe8SogFR7i9ufblMeYaDI4jtB19sZdHcTA2AJx0eOxvt7isge65Y68n3zv+3HrpkclExNj6mZjEG87sxk0vDsuaJBaV+LShlDUtmB/dhdA+LRUAqqHUhNVFB+J4StXHtk4fFXkOW0RwWEY6qwxuX/GDocN4Ss+nVcTmhBCC8lNjYLDjztxwuNiCSOyuEb+BRKb3/Kv/rcUEeBoO2xNFTG199zleVIiKd6F3foWS/pdH6B0v1/XVuiZMcCUZHceyUZTc9hJhd',
-	require => [User['batman'],File['/home/batman/.shh']],
+	require => File['/home/batman/.shh'],
   }
 }
