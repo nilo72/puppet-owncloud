ba20b6efa6796a0c096e139362152a099e9b8a79
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2015-06-11 11:17:19 +0200

    add minimal facts for module apt

--- spec/classes/owncloud_spec.rb
+++ spec/classes/owncloud_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 describe 'owncloud' do
   let(:node) { 'testhost.example.org' }
+
   let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+
+  let(:facts) { {
+    :osfamily        => 'Debian',
+    :operatingsystem => 'Debian',
+    :lsbdistid       => 'Debian',
+  } }
 
   context 'with default settngs' do
     it do
