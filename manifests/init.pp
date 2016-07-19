e07e5bbfaa81c5b058267ecc8b62a84722aa085a
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2016-07-19 15:28:44 +0200

    double defined parameters removed from init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -127,12 +127,11 @@ class owncloud (
   $do_Update                                  = $owncloud::params::do_Update,
   $enterprise_community                       = $owncloud::params::enterprise_community,
   $fqdn                                       = $::fqdn,
-  $installed                                  = $owncloud::params::installed,
-  $version                                    = $owncloud::params::version,
   $adminlogin                                 = $owncloud::params::apdminlogin,
   $adminpass                                  = $owncloud::params::adminpass,
   $clusternode                                = $owncloud::params::clusternode,
-) inherits owncloud::params{
+) inherits owncloud::params
+{
 
   # TODO: validate parameters
   validate_string($service_ensure)
